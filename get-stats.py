db8510259d0972bebedf8bde0816ad3ab599c4d0
Phil McDaniel <mcdanipc@gmail.com>
2021-01-14 11:37:54 -0600

    update for different game series
    update for different game series

--- get-stats.py
+++ get-stats.py
@@ -28,14 +28,11 @@ def download_file(url, filename):
 #download single file location
 
 urls = []
-urls.append('37cb2c52-676a-4a07-a16f-3605e47beb6b')
-urls.append('0f11948f-e73d-448d-9a14-bc687e77156c')
-urls.append('09772d90-acc7-4692-8e2f-fbb81aaf110e')
-urls.append('5d3a843a-62c8-46a7-9bd3-c86e754e5a7d')
-urls.append('83518fa7-9bb7-41c0-b192-2c7f7e96ef40')
-urls.append('70ef7a52-6014-4ec4-8f63-63dc526338e8')
-urls.append('9d053ab6-fd45-4e29-bd90-bdba4dec0629')
-urls.append('b399c2f1-14c1-4419-9b56-2dcba8038a63')
+urls.append('b76a4b9b-4ee5-4acf-932c-0fa552d3df07')
+urls.append('4ba80221-4a36-4b3d-a11d-d808c3f51836')
+urls.append('3c1bede4-0449-42ed-b4e6-9a20cb3cdc23')
+urls.append('bfc92b44-3310-4bf1-99da-e4018b958f33')
+
 
 #initalize summary dataframes
 
