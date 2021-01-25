be2bde86a0d0bccb106601dff6103c731dd8c85a
Phil McDaniel <mcdanipc@gmail.com>
2021-01-25 13:59:22 -0600

    added game date
    added game date

--- get-stats.py
+++ get-stats.py
@@ -27,78 +27,78 @@ def download_file(url, filename):
 
 urls = []
 #1/8 CLMN preseason tournament
-urls.append([['dbcb5251-b75c-4e29-a161-38ebde9418d9'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['9eda6262-6fea-46e2-aa7e-ab4a6d82d0ba'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['79975f63-9f7e-4120-8102-86bce529bce1'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['ce507231-65a6-445f-b71f-576529926950'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['b0bccf89-964c-4006-80f9-a60054198e61'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['c71bf073-889f-4a2d-81bb-e2923ba1bf32'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['46ad1fb8-ef84-4246-9d72-87cb8962d5b3'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['3fa94855-8e96-4de4-aaea-3ccccf60cf72'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['0b8405e7-24d4-426c-a18f-a3661ba02aae'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['dbcb5251-b75c-4e29-a161-38ebde9418d9'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['9eda6262-6fea-46e2-aa7e-ab4a6d82d0ba'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['79975f63-9f7e-4120-8102-86bce529bce1'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['ce507231-65a6-445f-b71f-576529926950'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['b0bccf89-964c-4006-80f9-a60054198e61'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['c71bf073-889f-4a2d-81bb-e2923ba1bf32'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['46ad1fb8-ef84-4246-9d72-87cb8962d5b3'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['3fa94855-8e96-4de4-aaea-3ccccf60cf72'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
+urls.append([['0b8405e7-24d4-426c-a18f-a3661ba02aae'],['Week 0'],['Preseason'],['Series 1'],['2020-01-08']])
 #1/11 boosty boys scrim
-urls.append([['b399c2f1-14c1-4419-9b56-2dcba8038a63'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['9d053ab6-fd45-4e29-bd90-bdba4dec0629'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['70ef7a52-6014-4ec4-8f63-63dc526338e8'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['83518fa7-9bb7-41c0-b192-2c7f7e96ef40'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['5d3a843a-62c8-46a7-9bd3-c86e754e5a7d'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['09772d90-acc7-4692-8e2f-fbb81aaf110e'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['0f11948f-e73d-448d-9a14-bc687e77156c'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['37cb2c52-676a-4a07-a16f-3605e47beb6b'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['b399c2f1-14c1-4419-9b56-2dcba8038a63'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['9d053ab6-fd45-4e29-bd90-bdba4dec0629'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['70ef7a52-6014-4ec4-8f63-63dc526338e8'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['83518fa7-9bb7-41c0-b192-2c7f7e96ef40'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['5d3a843a-62c8-46a7-9bd3-c86e754e5a7d'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['09772d90-acc7-4692-8e2f-fbb81aaf110e'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['0f11948f-e73d-448d-9a14-bc687e77156c'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['37cb2c52-676a-4a07-a16f-3605e47beb6b'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
 #1/11 firestorm scrim
-urls.append([['d5559778-1a20-4388-9d9b-a7770573d4cd'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['fabd2bf6-8a6b-4921-a7a8-c545e408df94'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['93007698-f2b4-4d23-958f-4438153a2570'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['8db975aa-4ea9-4e65-a8b8-0a9c6b34ce31'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['d5559778-1a20-4388-9d9b-a7770573d4cd'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['fabd2bf6-8a6b-4921-a7a8-c545e408df94'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['93007698-f2b4-4d23-958f-4438153a2570'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
+urls.append([['8db975aa-4ea9-4e65-a8b8-0a9c6b34ce31'],['Week 0'],['Preseason'],['Series 1'],['2020-01-11']])
 #1/13 firestorm scrim
-urls.append([['b76a4b9b-4ee5-4acf-932c-0fa552d3df07'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['4ba80221-4a36-4b3d-a11d-d808c3f51836'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['3c1bede4-0449-42ed-b4e6-9a20cb3cdc23'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['bfc92b44-3310-4bf1-99da-e4018b958f33'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['b76a4b9b-4ee5-4acf-932c-0fa552d3df07'],['Week 0'],['Preseason'],['Series 1'],['2020-01-13']])
+urls.append([['4ba80221-4a36-4b3d-a11d-d808c3f51836'],['Week 0'],['Preseason'],['Series 1'],['2020-01-13']])
+urls.append([['3c1bede4-0449-42ed-b4e6-9a20cb3cdc23'],['Week 0'],['Preseason'],['Series 1'],['2020-01-13']])
+urls.append([['bfc92b44-3310-4bf1-99da-e4018b958f33'],['Week 0'],['Preseason'],['Series 1'],['2020-01-13']])
 #1/15 week 0 scrims (only our games)
-urls.append([['708f160c-bd3b-435f-aeb9-ac9c7eb618c9'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['b7b86822-30df-4652-b4d4-26df4b564e7e'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['02bcc596-2b0e-43d3-b594-086e1f689952'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['708f160c-bd3b-435f-aeb9-ac9c7eb618c9'],['Week 0'],['Preseason'],['Series 1'],['2020-01-15']])
+urls.append([['b7b86822-30df-4652-b4d4-26df4b564e7e'],['Week 0'],['Preseason'],['Series 1'],['2020-01-15']])
+urls.append([['02bcc596-2b0e-43d3-b594-086e1f689952'],['Week 0'],['Preseason'],['Series 1'],['2020-01-15']])
 #1/19 duluth scrim
-urls.append([['8646c892-b61c-4508-85ed-ea84debea4f6'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['9d379e43-1fa7-4dee-9fa4-b401cd7386ce'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['b40638dc-6b6f-4ca3-b904-33022cf33037'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['be52e085-e6df-4061-8931-af293f306da2'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['a75dd6d3-0d88-4da9-aea3-661207b2137e'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['e5baf9d9-499a-44a8-9063-39771bbe060b'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['a4dc8bec-643a-4cdc-8956-e0fdb39a87c6'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['f6a11545-62f6-474c-b92a-8da7a1e2d6f4'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['8646c892-b61c-4508-85ed-ea84debea4f6'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['9d379e43-1fa7-4dee-9fa4-b401cd7386ce'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['b40638dc-6b6f-4ca3-b904-33022cf33037'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['be52e085-e6df-4061-8931-af293f306da2'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['a75dd6d3-0d88-4da9-aea3-661207b2137e'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['e5baf9d9-499a-44a8-9063-39771bbe060b'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['a4dc8bec-643a-4cdc-8956-e0fdb39a87c6'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
+urls.append([['f6a11545-62f6-474c-b92a-8da7a1e2d6f4'],['Week 0'],['Preseason'],['Series 1'],['2020-01-19']])
 #1/20 minneapolis scrim
-urls.append([['a1e6175c-435c-4a10-aa13-354745b96ff2'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['c313b774-aef0-4a4d-ba58-b7722f8697f5'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['774e81d0-7875-4b98-94ba-e65ebe98391e'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['8bf10bbe-9a9b-43c3-988d-d6c5c9187b16'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['7dc98c4e-6406-495c-9a3b-65c8a3e6b506'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['a1e6175c-435c-4a10-aa13-354745b96ff2'],['Week 0'],['Preseason'],['Series 1'],['2020-01-20']])
+urls.append([['c313b774-aef0-4a4d-ba58-b7722f8697f5'],['Week 0'],['Preseason'],['Series 1'],['2020-01-20']])
+urls.append([['774e81d0-7875-4b98-94ba-e65ebe98391e'],['Week 0'],['Preseason'],['Series 1'],['2020-01-20']])
+urls.append([['8bf10bbe-9a9b-43c3-988d-d6c5c9187b16'],['Week 0'],['Preseason'],['Series 1'],['2020-01-20']])
+urls.append([['7dc98c4e-6406-495c-9a3b-65c8a3e6b506'],['Week 0'],['Preseason'],['Series 1'],['2020-01-20']])
 #1/22 minnetonka scrim
-urls.append([['e5f81d7f-786e-42b3-859a-582579551f86'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['d690b8be-b3ab-4479-8fde-0d5acc635624'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['e11f9f67-e39a-4325-990a-39d8678861d8'],['Week 0'],['Preseason'],['Series 1']])
-urls.append([['1fe48305-2676-40ea-bc68-cff9541dcb85'],['Week 0'],['Preseason'],['Series 1']])
+urls.append([['e5f81d7f-786e-42b3-859a-582579551f86'],['Week 0'],['Preseason'],['Series 1'],['2020-01-22']])
+urls.append([['d690b8be-b3ab-4479-8fde-0d5acc635624'],['Week 0'],['Preseason'],['Series 1'],['2020-01-22']])
+urls.append([['e11f9f67-e39a-4325-990a-39d8678861d8'],['Week 0'],['Preseason'],['Series 1'],['2020-01-22']])
+urls.append([['1fe48305-2676-40ea-bc68-cff9541dcb85'],['Week 0'],['Preseason'],['Series 1'],['2020-01-22']])
 
 
 #1/22 CLMN Regular Season week 1
-urls.append([['de9d9a41-7a06-4084-8093-b43ab2ea3d67'],['Week 1'],['Regular Season'],['Series 1']])
-urls.append([['38d4dd5d-6cf3-4554-a826-da2e070776ec'],['Week 1'],['Regular Season'],['Series 1']])
-urls.append([['5c708b5a-2330-4edf-81ed-ba5946be4c5d'],['Week 1'],['Regular Season'],['Series 1']])
-urls.append([['4ff7d3ce-cfab-49c3-8f96-111ac96c5481'],['Week 1'],['Regular Season'],['Series 2']])
-urls.append([['d3cac342-a727-407b-8007-e93364c4b24a'],['Week 1'],['Regular Season'],['Series 2']])
-urls.append([['76bc0f03-373b-41ce-a47a-953db7dbcd93'],['Week 1'],['Regular Season'],['Series 2']])
-urls.append([['de3321cd-253a-42dd-b60c-b129785961d7'],['Week 1'],['Regular Season'],['Series 2']])
-urls.append([['0ac4f9ee-9bd6-432c-8386-e2359834c365'],['Week 1'],['Regular Season'],['Series 2']])
-urls.append([['d4e5f817-0325-4ed1-bf42-b0b46fcd2718'],['Week 1'],['Regular Season'],['Series 3']])
-urls.append([['938ed5ec-298e-493e-bd08-9cd6681145d1'],['Week 1'],['Regular Season'],['Series 3']])
-urls.append([['d768f0eb-d370-4318-8edd-114c67f0e5a5'],['Week 1'],['Regular Season'],['Series 3']])
-urls.append([['049b5978-7c06-461a-b0d9-3c86033692f2'],['Week 1'],['Regular Season'],['Series 4']])
-urls.append([['605e8fa3-5537-4d7b-8803-81fa037a59af'],['Week 1'],['Regular Season'],['Series 4']])
-urls.append([['f01338af-b445-4297-bf1c-7fbd39c8b05b'],['Week 1'],['Regular Season'],['Series 4']])
-urls.append([['321d66b0-4593-4afe-a926-9222d6959265'],['Week 1'],['Regular Season'],['Series 5']])
-urls.append([['2f95aa02-65a5-4d22-9d85-39bd8cd8a4dc'],['Week 1'],['Regular Season'],['Series 5']])
-urls.append([['616e136b-a899-429a-a29b-cd4b6f1acce4'],['Week 1'],['Regular Season'],['Series 5']])
+urls.append([['de9d9a41-7a06-4084-8093-b43ab2ea3d67'],['Week 1'],['Regular Season'],['Series 1'],['2020-01-22']])
+urls.append([['38d4dd5d-6cf3-4554-a826-da2e070776ec'],['Week 1'],['Regular Season'],['Series 1'],['2020-01-22']])
+urls.append([['5c708b5a-2330-4edf-81ed-ba5946be4c5d'],['Week 1'],['Regular Season'],['Series 1'],['2020-01-22']])
+urls.append([['4ff7d3ce-cfab-49c3-8f96-111ac96c5481'],['Week 1'],['Regular Season'],['Series 2'],['2020-01-22']])
+urls.append([['d3cac342-a727-407b-8007-e93364c4b24a'],['Week 1'],['Regular Season'],['Series 2'],['2020-01-22']])
+urls.append([['76bc0f03-373b-41ce-a47a-953db7dbcd93'],['Week 1'],['Regular Season'],['Series 2'],['2020-01-22']])
+urls.append([['de3321cd-253a-42dd-b60c-b129785961d7'],['Week 1'],['Regular Season'],['Series 2'],['2020-01-22']])
+urls.append([['0ac4f9ee-9bd6-432c-8386-e2359834c365'],['Week 1'],['Regular Season'],['Series 2'],['2020-01-22']])
+urls.append([['d4e5f817-0325-4ed1-bf42-b0b46fcd2718'],['Week 1'],['Regular Season'],['Series 3'],['2020-01-22']])
+urls.append([['938ed5ec-298e-493e-bd08-9cd6681145d1'],['Week 1'],['Regular Season'],['Series 3'],['2020-01-22']])
+urls.append([['d768f0eb-d370-4318-8edd-114c67f0e5a5'],['Week 1'],['Regular Season'],['Series 3'],['2020-01-22']])
+urls.append([['049b5978-7c06-461a-b0d9-3c86033692f2'],['Week 1'],['Regular Season'],['Series 4'],['2020-01-22']])
+urls.append([['605e8fa3-5537-4d7b-8803-81fa037a59af'],['Week 1'],['Regular Season'],['Series 4'],['2020-01-22']])
+urls.append([['f01338af-b445-4297-bf1c-7fbd39c8b05b'],['Week 1'],['Regular Season'],['Series 4'],['2020-01-22']])
+urls.append([['321d66b0-4593-4afe-a926-9222d6959265'],['Week 1'],['Regular Season'],['Series 5'],['2020-01-22']])
+urls.append([['2f95aa02-65a5-4d22-9d85-39bd8cd8a4dc'],['Week 1'],['Regular Season'],['Series 5'],['2020-01-22']])
+urls.append([['616e136b-a899-429a-a29b-cd4b6f1acce4'],['Week 1'],['Regular Season'],['Series 5'],['2020-01-22']])
 
 #print(urls)
 
@@ -140,6 +140,9 @@ for url in urls:
 
     #add column for week number
     playerdata['Week Number'] = url[1][0]
+    
+    #add column for game date
+    playerdata['Game Date'] = url[4][0]
     
     #write back to csv
     playerdata.to_csv(playerfile, sep=';', encoding='utf-8',index=False)
@@ -195,6 +198,9 @@ for url in urls:
 
     #add column for week number
     teamdata['Week Number'] = url[1][0]
+
+    #add column for game date
+    teamdata['Game Date'] = url[4][0]
 
     #add column for week number
     teamdata['Series Number'] = url[3][0]
@@ -257,7 +263,7 @@ teamsummary
 
 
 #game summary
-gameresults = teamsummary[['color','Game','Result','team name','Week Number','Series']]
+gameresults = teamsummary[['color','Game','Result','team name','Week Number','Series Number']]
 gameresults
 
 playersummary = pd.merge(playersummary, gameresults, on=['color', 'Game'])
