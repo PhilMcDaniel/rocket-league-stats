34ee1a044bbf21e7f7165db04bff701cbbb82b95
Phil McDaniel <mcdanipc@gmail.com>
2021-02-14 17:42:26 -0600

    add summary tables to site
    Add additional sites pages to site

--- main.py
+++ main.py
@@ -4,15 +4,25 @@ import pandas as pd
 app = Flask(__name__)
 
 #get dataframe with data
-df = pd.read_csv(".\\stat_files\\PLAYER_0ac4f9ee-9bd6-432c-8386-e2359834c365.csv", sep=';')
+player_summary = pd.read_csv(".\\stat_files\\summary\\CLMNPlayerSummary.csv", sep=';')
+overall_records = pd.read_csv(".\\stat_files\\summary\\CLMNSeriesRecord.csv", sep=';')
+team_summary = pd.read_csv(".\\stat_files\\summary\\CLMNTeamSummary.csv", sep=';')
 
 @app.route("/")
 def home():
-    return render_template("home.html",tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template("home.html")
     
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route("/overallrecords")
+def overallrecord():
+    return render_template("home.html",tables=[overall_records.to_html(classes='data')],titles=overall_records.columns.values)
+
+@app.route("/teamsummary")
+def teamsummary():
+    return render_template("home.html",tables=[team_summary.to_html(classes='data')],titles=team_summary.columns.values)
+
+@app.route("/playersummary")
+def playersummary():
+    return render_template("home.html",tables=[player_summary.to_html(classes='data')],titles=player_summary.columns.values)
     
 if __name__ == "__main__":
     app.run(debug=True)
