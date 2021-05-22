49b3976b371e1de743a5218236c59c8fc0bd35aa
Phil McDaniel <mcdanipc@gmail.com>
2021-05-22 17:03:28 -0500

    Update match_result_simulation.py
    Parameterize script for two team names

--- personal-python/match_result_simulation.py
+++ personal-python/match_result_simulation.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import os,sys
 
-
-
 #import team data
 team_stats = pd.read_csv("personal-python/data.csv", sep=',')
 team_stats
@@ -11,24 +9,31 @@ team_stats
 #generate a schedule of matches where everyone plays eachother twice
 
 #generate simulated values based on mean and stddev
-number_of_sims = 5
+number_of_sims = 100000
 
-goals_weight = 0.5
-goals_allowed_weight = 0.5
+goals_weight = .5
+goals_allowed_weight = .5
 
-team1_goals_mean = 1
-team1_goals_stddev = 1
-team1_goals_allowed_mean = 2 
-team1_goals_allowed_stddev = 1
-team2_goals_mean = 2
-team2_goals_stddev = 1
-team2_goals_allowed_mean = 1
-team2_goals_allowed_stddev = 1
+team1 = 'Bemidji Beavers'
+team2 = 'Bemidji Beavers'
 
+#get team 1 values
+team1_goals_mean = team_stats['Goals/Game'][team_stats["Team Name"] == team1].values[0]
+team1_goals_stddev = team_stats['Std Dev Goals'][team_stats["Team Name"] == team1].values[0]
+team1_goals_allowed_mean = team_stats['Goals Against/Game'][team_stats["Team Name"] == team1].values[0]
+team1_goals_allowed_stddev = team_stats['Std Dev Goals Against'][team_stats["Team Name"] == team1].values[0]
+
+#get team 2 values
+team2_goals_mean = team_stats['Goals/Game'][team_stats["Team Name"] == team2].values[0]
+team2_goals_stddev = team_stats['Std Dev Goals'][team_stats["Team Name"] == team2].values[0]
+team2_goals_allowed_mean = team_stats['Goals Against/Game'][team_stats["Team Name"] == team2].values[0]
+team2_goals_allowed_stddev = team_stats['Std Dev Goals Against'][team_stats["Team Name"] == team2].values[0]
+
+#generate random data
 team1_goals = np.random.normal(team1_goals_mean,team1_goals_stddev,number_of_sims)
 team1_goals_allowed = np.random.normal(team1_goals_allowed_mean,team1_goals_allowed_stddev,number_of_sims)
 team2_goals = np.random.normal(team2_goals_mean,team2_goals_stddev,number_of_sims)
-team2_goals_allowed = np.random.normal(team2_goals_allowed_mean,team2_goals_stddev,number_of_sims)
+team2_goals_allowed = np.random.normal(team2_goals_allowed_mean,team2_goals_allowed_stddev,number_of_sims)
 
 #weigh goals and goals against for opponent to get estimated goals for each team
 team1_goal_estimate = (team1_goals * goals_weight) + (team2_goals_allowed * goals_allowed_weight)
@@ -42,6 +47,7 @@ print(team1_result_wins)
 team1_wins = np.count_nonzero(team1_result_wins)
 team1_losses = np.size(team1_result_wins) - np.count_nonzero(team1_result_wins)
 team1_win_pct = team1_wins / (team1_wins + team1_losses)
-print(f"Team 1 - Wins: {team1_wins}, Losses: {team1_losses}, Win %: {team1_win_pct}")
-
+team1_loss_pct = team1_losses / (team1_wins + team1_losses)
+print(f"{team1} - Wins: {team1_wins}, Losses: {team1_losses}, Win %: {team1_win_pct}")
+print(f"{team2} - Wins: {team1_losses}, Losses: {team1_wins}, Win %: {team1_loss_pct}")
 #show summarized totals throughout the simulated season
