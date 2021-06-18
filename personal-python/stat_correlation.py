03841c1ff33297e77a21df76440d730ea10f0923
Phil McDaniel <mcdanipc@gmail.com>
2021-06-18 16:04:47 -0500

    Update stat_correlation.py
    Add script to show variable correlation

--- personal-python/stat_correlation.py
+++ personal-python/stat_correlation.py
@@ -1,3 +1,26 @@
 import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
+
+
+all_player_stats = pd.read_csv("MNCS/MNCS_S3_PlayerRatingData.csv", sep=',')
+MNCS_player_stats = all_player_stats[all_player_stats['League'] == 'mncs']
+CLMN1_player_stats = all_player_stats[all_player_stats['League'] == 'clmn 1']
+CLMN2_player_stats = all_player_stats[all_player_stats['League'] == 'clmn 2']
+
+all_players_corrMatrix = all_player_stats.corr()
+MNCS_corrMatrix = MNCS_player_stats.corr()
+CLMN1_corrMatrix = CLMN1_player_stats.corr()
+CLMN2_corrMatrix = CLMN2_player_stats.corr()
+
+
+print(all_players_corrMatrix,MNCS_corrMatrix,CLMN1_corrMatrix,CLMN2_corrMatrix)
+
+sn.heatmap(all_players_corrMatrix, annot=True)
+plt.show()
+sn.heatmap(MNCS_corrMatrix, annot=True)
+plt.show()
+sn.heatmap(CLMN1_corrMatrix, annot=True)
+plt.show()
+sn.heatmap(CLMN2_corrMatrix, annot=True)
+plt.show()
