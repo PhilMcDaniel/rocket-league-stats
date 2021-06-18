7473935b5202cc1a0f947e465f06f51ac42a9531
Phil McDaniel <mcdanipc@gmail.com>
2021-06-18 16:07:10 -0500

    Update stat_correlation.py
    comments and fix the Game Win % datatype

--- personal-python/stat_correlation.py
+++ personal-python/stat_correlation.py
@@ -2,8 +2,11 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-
+#read csv source file
 all_player_stats = pd.read_csv("MNCS/MNCS_S3_PlayerRatingData.csv", sep=',')
+#change data type to numeric so correlation works
+all_player_stats['Game Win %'] = all_player_stats['Game Win %'].str.replace('%','').astype(float)
+
 MNCS_player_stats = all_player_stats[all_player_stats['League'] == 'mncs']
 CLMN1_player_stats = all_player_stats[all_player_stats['League'] == 'clmn 1']
 CLMN2_player_stats = all_player_stats[all_player_stats['League'] == 'clmn 2']
@@ -12,7 +15,6 @@ all_players_corrMatrix = all_player_stats.corr()
 MNCS_corrMatrix = MNCS_player_stats.corr()
 CLMN1_corrMatrix = CLMN1_player_stats.corr()
 CLMN2_corrMatrix = CLMN2_player_stats.corr()
-
 
 print(all_players_corrMatrix,MNCS_corrMatrix,CLMN1_corrMatrix,CLMN2_corrMatrix)
 
