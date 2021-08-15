c73ae038e9727422c935932633cba6b17892d4f3
Phil McDaniel <mcdanipc@gmail.com>
2021-08-15 18:44:12 -0500

    Update Batch.sql
    Add ETL_DTM

--- personal-python/azure/DatabaseProjectRocketLeagueStats/dbo/Tables/Batch.sql
+++ personal-python/azure/DatabaseProjectRocketLeagueStats/dbo/Tables/Batch.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Batch] (
     [Batch_Id]     UNIQUEIDENTIFIER NOT NULL,
     [BatchDate]    DATE             NULL,
+    [ETL_DTM]      DATETIME2 (7) NULL,
     [BatchDurationSeconds] DECIMAL (13,4)  NULL,
     PRIMARY KEY CLUSTERED (Batch_Id ASC)
 )
