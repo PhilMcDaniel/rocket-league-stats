576d72ecfedf3032759aebf08eca615d1376a314
aeronut343 <halex211@umn.edu>
2022-07-06 15:14:36 -0500

    Updated Salaries

--- personal-python/stat_correlation.py
+++ personal-python/stat_correlation.py
@@ -3,21 +3,21 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 
 #read csv source file
-all_player_stats = pd.read_csv("d:/Git/MNCS/rocket-league-stats/personal-python/s4_coeffs.csv", sep=',')
+all_player_stats = pd.read_csv("d:/Git/MNCS/rocket-league-stats/personal-python/s5_data.csv", sep=',')
 #change data type to numeric so correlation works
 all_player_stats['Game Win %'] = all_player_stats['player game win %'].str.replace('%','').astype(float)
 
-MNCS_player_stats = all_player_stats[all_player_stats['league_name'] == 'mncs']
-CLMN_player_stats = all_player_stats[all_player_stats['league_name'] == 'clmn']
-MNRS_player_stats = all_player_stats[all_player_stats['league_name'] == 'mnrs']
+Premier_player_stats = all_player_stats[all_player_stats['league_name'] == 'Premier']
+Challenger_player_stats = all_player_stats[all_player_stats['league_name'] == 'Challenger']
+RisingStar_player_stats = all_player_stats[all_player_stats['league_name'] == 'RisingStar']
 
 all_players_corrMatrix = all_player_stats.corr()
-MNCS_corrMatrix = MNCS_player_stats.corr()
-CLMN_corrMatrix = CLMN_player_stats.corr()
-MNRS_corrMatrix = MNRS_player_stats.corr()
+Premier_corrMatrix = Premier_player_stats.corr()
+Challenger_corrMatrix = Challenger_player_stats.corr()
+RisingStar_corrMatrix = RisingStar_player_stats.corr()
 
-print(MNRS_corrMatrix)
-#print(all_players_corrMatrix,MNCS_corrMatrix,CLMN_corrMatrix,MNRS_corrMatrix)
+#print(Premier_corrMatrix)
+print(all_players_corrMatrix,Premier_corrMatrix,Challenger_corrMatrix,RisingStar_corrMatrix)
 
 # sn.heatmap(all_players_corrMatrix, annot=True)
 # plt.show()
