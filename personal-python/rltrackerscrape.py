14ad35053ad3515c952c242fe27f76e2b64a86c4
Phil McDaniel <mcdanipc@gmail.com>
2022-12-30 12:10:41 -0600

    Update rltrackerscrape.py

--- personal-python/rltrackerscrape.py
+++ personal-python/rltrackerscrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 from requests import api
+from datetime import datetime
 
 def form_url(platform,platformid):
     """Get the URL that needs to be parsed"""
@@ -22,21 +23,44 @@ def get_rank_from_api(url):
     }
     api_response = requests.get(url,headers=headers)
     api_response = api_response.json()
+
+    platform = api_response['data']['platformInfo']['platformSlug']
+    platform_handle = api_response['data']['platformInfo']['platformUserHandle']
+    platform_handle_id = api_response['data']['platformInfo']['platformUserIdentifier']
     
-    #get user info
+    data = {}
+    for row in api_response['data']['segments']:
+        stat_type = [row][0]['type']
+        #this row has different keys so skip it
+        if stat_type == 'overview':
+            continue
+        season = [row][0]['attributes']['season']
+        playlist_name = [row][0]['metadata']['name']
+        rank = [row][0]['stats']['tier']['metadata']['name']
+        division_name = [row][0]['stats']['division']['metadata']['name']
+        season_matches_played = [row][0]['stats']['matchesPlayed']['value']
+        win_streak = int([row][0]['stats']['winStreak']['displayValue'])
+        rating_value = int([row][0]['stats']['rating']['value'])
+        dt = str(datetime.now())
+           
+        data[(platform_handle_id,playlist_name,dt)] = {"platform_handle_id":platform_handle_id,"platform_handle":platform_handle,"datetime":dt,"platform":platform,"platform_handle":platform_handle,"stat_type":stat_type,"season":season,"playlist_name":playlist_name,"rank":rank,"division_name":division_name,"season_matches_played":season_matches_played,"win_streak":win_streak,"rating_value":rating_value}
     
-    return(api_response)
+    return(data)
+
 result = get_rank_from_api(form_url('steam','76561198040589211'))
 
-result['data'].keys()
 
-result['data']['segments'][3].keys()
-result['data']['segments'][3]['metadata']['name']
+#result[('76561198040589211', 'Snowday', '2022-12-30 12:07:56.773688')]
 
+#df = pd.DataFrame.from_dict(result)
+#df.head()
 
-result['data']['segments'][3]['stats'].keys()
-result['data']['segments'][3]['stats']['rating']['value']
+#convert to dataframe that is flattened correctly
 
+#write to csv
 
-df = pd.DataFrame.from_dict(result['data']['segments'])
-df.head()
+#read file of platform / platform id
+
+#main program to run these methods
+
+#dag
